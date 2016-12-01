1709f83247615457a4c950870882854afdfcf136
sudhakarkr <sudhakarkr@HCL-Sudhakar.local>
2016-12-01 23:48:06 +0000

    Changes and Initial Comming

---
+++ microservice-demo-catalog/src/test/java/com/lego/microservice/catalog/RepositoryTest.java
@@ -0,0 +1,33 @@
+package com.lego.microservice.catalog;
+
+import static org.hamcrest.Matchers.*;
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import com.lego.microservice.catalog.CatalogApp;
+import com.lego.microservice.catalog.ItemRepository;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = CatalogApp.class, webEnvironment = WebEnvironment.NONE)
+@ActiveProfiles("test")
+public class RepositoryTest {
+
+	@Autowired
+	private ItemRepository itemRepository;
+
+	@Test
+	public void AreAllIPodReturned() {
+
+		assertThat(itemRepository.findByNameContaining("iPod"), hasSize(3));
+		assertTrue(
+				itemRepository.findByNameContaining("iPod").stream().anyMatch(s -> s.getName().equals("iPod touch")));
+
+	}
+}
